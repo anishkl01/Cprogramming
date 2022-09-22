4ede920f8408fce506a3e5b3a624d42d2ba68460
jiraiya0 <111779936+jiraiya0@users.noreply.github.com>
2022-09-23 01:16:34 +0530

    Create fahrenheit.c

---
+++ fahrenheit.c
@@ -0,0 +1,10 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+float celsius, fahrenheit;
+
+void main() {
+  printf("Enter temperature in Fahrenheit:");
+  scanf("%f",&fahrenheit);
+  celsius = (fahrenheit-32)*5/9;
+  printf("ce
